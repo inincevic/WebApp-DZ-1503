d8dbf035f660ca2d455a13558c0b70f15d09ea7b
inincevic <inincevic@unipu.hr>
2022-03-17 07:59:52 +0100

    Changed about to details and started editing Home

---
+++ src/views/Details.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Details">
+    <h1>This is the page on which commit details will be shown.</h1>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to an App that shows git commits."/>
   </div>
 </template>
 
@@ -15,4 +14,5 @@ export default {
     HelloWorld
   }
 }
+
 </script>
