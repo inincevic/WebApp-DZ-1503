b287aabd6743adeb7a2e4fcb30872ca39e68903a
inincevic <inincevic@unipu.hr>
2022-03-17 09:24:01 +0100

    Addon 1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to an App that shows git commits."/>
+    <GetCommits/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import GetCommits from '@/components/GetCommits.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    GetCommits
   }
 }
 
